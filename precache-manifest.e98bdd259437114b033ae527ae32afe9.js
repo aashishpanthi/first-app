a8f97756d4c942d4f1abaa6c51b781eb6e9e6491
Aashish Panthi <aashishpanthi11@gmail.com>
2020-09-07 23:34:39 +0545

    Updates

---
+++ precache-manifest.e98bdd259437114b033ae527ae32afe9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8565f3d2ab549e928b3ea3a36984c63a",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "dad01b89d06fea7b590e",
+    "url": "/first-app/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "b69e66669c0f23757dde",
+    "url": "/first-app/static/css/main.ade81ddf.chunk.css"
+  },
+  {
+    "revision": "dad01b89d06fea7b590e",
+    "url": "/first-app/static/js/2.796a62f1.chunk.js"
+  },
+  {
+    "revision": "329be32b7d5199e4dc4e1dda122faca8",
+    "url": "/first-app/static/js/2.796a62f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b69e66669c0f23757dde",
+    "url": "/first-app/static/js/main.29a1e5c9.chunk.js"
+  },
+  {
+    "revision": "0411795b630819b8d466",
+    "url": "/first-app/static/js/runtime-main.284ef3e7.js"
+  },
+  {
+    "revision": "3a8c0d8be7fe574d48f58646c2915662",
+    "url": "/first-app/static/media/blog.3a8c0d8b.jpg"
+  },
+  {
+    "revision": "9e6c6c495c4b2bee06598974534ad11e",
+    "url": "/first-app/static/media/creative.9e6c6c49.jpg"
+  },
+  {
+    "revision": "061d7f040f884d5e22f3562b4bb95c56",
+    "url": "/first-app/static/media/creative2.061d7f04.jpg"
+  },
+  {
+    "revision": "d2b3d26ce1244d779a1b672d00aaef0d",
+    "url": "/first-app/static/media/photo.d2b3d26c.png"
+  },
+  {
+    "revision": "36cb1a868c128048c239197110df75a2",
+    "url": "/first-app/static/media/software.36cb1a86.jpg"
+  },
+  {
+    "revision": "04c288234907fae5a9964c285479195e",
+    "url": "/first-app/static/media/videography.04c28823.jpg"
+  },
+  {
+    "revision": "a1bac2780cbc8b9a20c14cd01652d640",
+    "url": "/first-app/static/media/vlog.a1bac278.jpg"
+  },
+  {
+    "revision": "e73fafe78da512622f722a07471e6de7",
+    "url": "/first-app/static/media/youtube.e73fafe7.jpg"
+  }
+]);
